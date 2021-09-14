07e1f86845a8d555b8ed650abb7a2ca11bb8a79f
Tim Euf <cb2eufm@ztb.icb.commerzbank.com>
2021-09-14 11:29:27 +0000

    Test #6

--- ui.R
+++ ui.R
@@ -1,5 +1,5 @@
 ui <- fluidPage(
-  selectInput("dataset", label = "Dataset_v3", choices = ls("package:datasets")),
+  selectInput("dataset", label = "Dataset_v4_demo", choices = ls("package:datasets")),
   verbatimTextOutput("summary"),
   tableOutput("table")
 )
